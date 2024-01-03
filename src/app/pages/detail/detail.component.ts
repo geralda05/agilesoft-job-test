ec598647ae6e1ad0508099129c2607a452feb4cf
Gerald Alarcon <leonel.alarcon@tecnoandina.cl>
2024-01-03 00:24:47 -0300

    feat: detail changes

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../../services/data.service';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [CommonModule, SidebarComponent],
+  imports: [CommonModule, NgbCarouselModule, SidebarComponent],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss'
 })
@@ -19,12 +20,22 @@ export class DetailComponent {
   public movieInfo : {[k: string]: any} = {};
   public movieId : string = '';
   public baseURL: string = '';
+  public actors : any = [];
+  public actorsBase : string = '';
 
   receiveParameters() {
     const params = this.router.getCurrentNavigation()?.extras.state;
     if(params){
       this.movieInfo = params['movie'];
       this.baseURL = params['baseURL'];
+    }
+  }
+
+  public formatSlide(value : any) {
+    if(value){
+      return `background-image: url('${this.actorsBase+value.profile_path}'); background-position: center;`
+    } else {
+      return ''
     }
   }
 
@@ -39,6 +50,11 @@ export class DetailComponent {
 
   public async getDetailMovie() {
     const response = await this.data.getDetailMovie(this.movieId)
+    if(response.data) {
+      this.actors = response.data.data;
+      console.log(this.actors);
+      this.actorsBase = response.data.imageBaseUrl;
+    }
     console.log(response.data);
   }
 }
